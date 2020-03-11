4059c286613e1192b7e5ef14a28395bba67d37db
yohannaTML <47210862+yohannaTML@users.noreply.github.com>
2020-03-11 09:02:12 +0100

    Add comments

--- src/application/model/Movies.java
+++ src/application/model/Movies.java
@@ -2,6 +2,8 @@ package application.model;
 
 import java.util.Iterator;
 
+/* This is an interface where we create the iterator*/
+
 public interface Movies {
 	public Iterator<MovieInfo> createIterator();
 }
